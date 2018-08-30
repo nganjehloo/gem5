ab3135bd58c4d89418633948c6c19ae0397d731c
Jason Lowe-Power <jason@lowepower.com>
2018-08-30 16:51:12 -0700

    flexcpu-experiments: Reorg perf data
    Change-Id: Ib7c210f9e5da2b30627614f2d794399402174269
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- experiments/run_perf.sh
+++ experiments/run_perf.sh
@@ -5,6 +5,8 @@ bms=`find benchmarks -type d -links 2 | cut -f2 -d'/'`
 for D in $bms
 do
     cd benchmarks/$D
-    perf stat -e instructions,cpu-cycles ./$D > reference.out 2> ../../res/perf/$D
-    cd -
+    mkdir -p ../../res/perf/$D
+    echo "Running $D"
+    perf stat -x' ' -o ../../res/perf/$D/epyc7451 -e instructions,cpu-cycles ./$D > reference.out
+    cd - > /dev/null
 done
