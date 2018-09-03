1486697cd2371cdd9f316d3b4d1d62f36c8bdd24
Jason Lowe-Power <jason@lowepower.com>
2018-09-03 14:21:03 -0700

    flexcpu-experiments: Update perf script with u-op counters
    Change-Id: If955b618a23508175d2ae85601854a5be6cfcb58
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- experiments/run_perf.sh
+++ experiments/run_perf.sh
@@ -2,11 +2,16 @@
 
 bms=`find benchmarks -type d -links 2 | cut -f2 -d'/'`
 
+# u-ops stat
+# Zen (epyc7451): rc1
+# Skylake (i7-7700): uops_executed.thread
+# Sandybridge (E5-1607): uops_retired.all
+
 for D in $bms
 do
     cd benchmarks/$D
     mkdir -p ../../res/perf/$D
     echo "Running $D"
-    perf stat -x' ' -o ../../res/perf/$D/epyc7451 -e instructions,cpu-cycles ./$D > reference.out
+    perf stat -x' ' -o ../../res/perf/$D/epyc7451 -e instructions,cpu-cycles,rc1 ./$D > reference.out
     cd - > /dev/null
 done
